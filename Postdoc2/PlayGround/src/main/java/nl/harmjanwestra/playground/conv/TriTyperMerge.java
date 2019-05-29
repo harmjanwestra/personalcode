030259101c291952fccfad6c692ca21ba6b10d10
harmjanwestra <westra.harmjan@outlook.com>
2019-05-29 10:50:39 +0200

    TriTyper Merger

--- Postdoc2/PlayGround/src/main/java/nl/harmjanwestra/playground/conv/TriTyperMerge.java
+++ Postdoc2/PlayGround/src/main/java/nl/harmjanwestra/playground/conv/TriTyperMerge.java
@@ -1,6 +1,11 @@
 package nl.harmjanwestra.playground.conv;
 
+import umcg.genetica.io.bin.BinaryFile;
 import umcg.genetica.io.text.TextFile;
+import umcg.genetica.io.trityper.SNP;
+import umcg.genetica.io.trityper.SNPLoader;
+import umcg.genetica.io.trityper.TriTyperGenotypeData;
+import umcg.genetica.io.trityper.util.BaseAnnot;
 
 import java.io.IOException;
 import java.util.*;
@@ -58,19 +63,190 @@ public class TriTyperMerge {
 		}
 	}
 
-	public void run(String[] folders, String[] names) throws IOException {
+	private byte convertDosageToByte(double dosageValue) {
+		int dosageInt = (int) Math.round(dosageValue * 100d);
+		byte dosageByte = (byte) (Byte.MIN_VALUE + dosageInt);
+		return dosageByte;
+	}
+
+	public void run(String[] folders, String[] names, String outdir) throws IOException {
 		// read SNPs
 		SNP[] snps = readAndFilterSNPs(folders);
 
 		// read individuals
 		ArrayList<String> individuals = new ArrayList<>();
+		TextFile indo = new TextFile(outdir + "Individuals.txt", TextFile.W);
+		TextFile pheno = new TextFile(outdir + "Individuals.txt", TextFile.W);
+
 		for (int f = 0; f < folders.length; f++) {
 			ArrayList<String> inds = readAsList(folders[f] + "Individuals.txt");
 			for (String s : inds) {
 				individuals.add(names[f] + "-" + s);
+				indo.writeln(names[f] + "-" + s);
+				pheno.writeln(names[f] + "-" + s );
 			}
 		}
+		pheno.close();
+		indo.close();
 
+		// now we have the dimensions of the new matrix
+		TriTyperGenotypeData[] datasets = new TriTyperGenotypeData[folders.length];
+		SNPLoader[] loaders = new SNPLoader[folders.length];
+		IntStream.range(0, folders.length).parallel().forEach(i -> {
+			try {
+				datasets[i] = new TriTyperGenotypeData(folders[i]);
+				loaders[i] = datasets[i].createSNPLoader(1);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+
+
+		byte[] genotypesA1 = new byte[individuals.size()];
+		byte[] genotypesA2 = new byte[individuals.size()];
+		byte[] dosages = new byte[individuals.size()];
+		int[] startint = new int[datasets.length];
+		for (int i = 1; i < startint.length; i++) {
+			startint[i] = datasets[i - 1].getIndividuals().length;
+		}
+
+
+		BinaryFile bfgt = new BinaryFile(outdir + "GenotypeMatrix.dat", BinaryFile.W);
+		BinaryFile bfds = new BinaryFile(outdir + "ImputedDosageMatrix.dat", BinaryFile.W);
+		TextFile snpo = new TextFile(outdir + "SNPs.txt.gz", TextFile.W);
+		TextFile snpmo = new TextFile(outdir + "SNPMappings.txt.gz", TextFile.W);
+
+		for (int i = 0; i < snps.length; i++) {
+
+			String snpname = snps[i].name;
+			for (int j = 0; j < individuals.size(); j++) {
+				genotypesA1[j] = -1;
+				genotypesA2[j] = -1;
+				dosages[j] = -1;
+			}
+
+
+			umcg.genetica.io.trityper.SNP referenceSNP = null;
+			String refalleles = null;
+			byte[] refallelesb = null;
+			String refminor = null;
+
+			for (int j = 0; j < datasets.length; j++) {
+				int id = datasets[i].getSnpToSNPId().get(snpname);
+				int starti = startint[j];
+				if (id > -1) {
+					umcg.genetica.io.trityper.SNP snpobj = datasets[i].getSNPObject(id);
+					loaders[i].loadGenotypes(snpobj);
+					if (loaders[i].hasDosageInformation()) {
+						loaders[i].loadDosage(snpobj);
+					}
+
+					if (referenceSNP == null) {
+						// set reference snp
+						referenceSNP = snpobj;
+
+						// copy alleles and dosage (if present)
+						System.arraycopy(snpobj.getAllele1(), 0, genotypesA1, starti, datasets[i].getIndividuals().length);
+						System.arraycopy(snpobj.getAllele2(), 0, genotypesA2, starti, datasets[i].getIndividuals().length);
+						refalleles = BaseAnnot.getAllelesDescription(snpobj.getAlleles());
+						refallelesb = snpobj.getAlleles();
+						refminor = BaseAnnot.toString(snpobj.getMinorAllele());
+
+						if (snpobj.hasDosageInformation()) {
+							double[] dsdosge = snpobj.getDosageValues();
+
+							for (int d = 0; d < dsdosge.length; d++) {
+								dosages[starti + d] = convertDosageToByte(dsdosge[d]);
+							}
+						} else {
+							// convert genotypes do dosage
+							byte[] genotypes = snpobj.getGenotypes();
+							for (int d = 0; d < genotypes.length; d++) {
+								dosages[starti + d] = convertDosageToByte(genotypes[d]);
+							}
+
+						}
+
+					} else {
+						// compare alleles
+						String alleles = BaseAnnot.getAllelesDescription(snpobj.getAlleles());
+						String minor = BaseAnnot.toString(snpobj.getMinorAllele());
+
+						Boolean flipallele = BaseAnnot.flipalleles(refalleles, refminor, alleles, minor);
+						if (flipallele != null) {
+							if (flipallele) {
+								// use genotypes to define alleles
+								byte[] genotypes = snpobj.getGenotypes();
+								double[] dsdosge = snpobj.getDosageValues();
+								for (int d = 0; d < genotypes.length; d++) {
+									if (genotypes[d] > -1) {
+										if (genotypes[d] == 0) {
+											genotypesA1[starti + d] = refallelesb[1];
+											genotypesA2[starti + d] = refallelesb[1];
+										} else if (genotypes[d] == 1) {
+											genotypesA1[starti + d] = refallelesb[0];
+											genotypesA2[starti + d] = refallelesb[1];
+										} else {
+											genotypesA1[starti + d] = refallelesb[0];
+											genotypesA2[starti + d] = refallelesb[0];
+										}
+									}
+									if (snpobj.hasDosageInformation()) {
+										if (dsdosge[d] != -1) {
+											dosages[starti + d] = convertDosageToByte(Math.abs(dsdosge[d] - 2));
+										}
+									} else {
+										if (genotypes[d] != -1) {
+											dosages[starti + d] = convertDosageToByte(Math.abs(genotypes[d] - 2));
+										}
+									}
+								}
+							} else {
+								// need to take into account allele encoding...
+								byte[] genotypes = snpobj.getGenotypes();
+								double[] dsdosge = snpobj.getDosageValues();
+								for (int d = 0; d < genotypes.length; d++) {
+									if (genotypes[d] > -1) {
+										if (genotypes[d] == 0) {
+											genotypesA1[starti + d] = refallelesb[0];
+											genotypesA2[starti + d] = refallelesb[0];
+										} else if (genotypes[d] == 1) {
+											genotypesA1[starti + d] = refallelesb[0];
+											genotypesA2[starti + d] = refallelesb[1];
+										} else {
+											genotypesA1[starti + d] = refallelesb[1];
+											genotypesA2[starti + d] = refallelesb[1];
+										}
+									}
+									if (snpobj.hasDosageInformation()) {
+										if (dsdosge[d] != -1) {
+											dosages[starti + d] = convertDosageToByte(dsdosge[d]);
+										}
+									} else {
+										if (genotypes[d] != -1) {
+											dosages[starti + d] = convertDosageToByte(genotypes[d]);
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+
+			// write result to disk
+			bfgt.write(genotypesA1);
+			bfgt.write(genotypesA2);
+			bfds.write(dosages);
+			snpo.writeln(snpname);
+			snpmo.writeln(snps[i].chr + "\t" + snps[i].pos + "\t" + snpname + "\t" + BaseAnnot.toString(refallelesb[0]) + "," + BaseAnnot.toString(refallelesb[1]));
+
+		}
+
+		// close writers
+		bfgt.close();
+		bfds.close();
+		snpo.close();
 
 	}
 
